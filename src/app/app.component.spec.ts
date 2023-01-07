b19aec13958b660a36b5c1378deb8ad052b3470a
Mateusz Piekny <m.piekny97@gmail.com>
2023-01-07 13:50:16 +0100

    Changed project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'netcap2'`, () => {
+  it(`should have as title 'netcap'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('netcap2');
+    expect(app.title).toEqual('netcap');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('netcap2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('netcap app is running!');
   });
 });
