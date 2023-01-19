b65523407ce1f0b243b4b3fc56b30e57de18d4b6
Mateusz Piekny <m.piekny97@gmail.com>
2023-01-19 18:35:38 +0100

    Added home page laptop component backend; updated gitignore

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,21 +1,48 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { AuthService } from "../../services/auth-service/auth.service";
 import { MatDialog } from '@angular/material/dialog';
 import { DialogLogoutComponent } from '../dialog-logout/dialog-logout.component';
+import { Laptop } from 'src/app/models/laptop/laptop.model';
+import { LaptopService } from 'src/app/services/laptop-service/laptop.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  laptops?: Laptop[] = new Array<Laptop>();
+
   constructor(
     public authService: AuthService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private laptopService: LaptopService
     ) { }
+
+  ngOnInit(): void {
+     this.retrieveLaptops();
+  }
 
   ShowDialog(){
     this.dialog.open(DialogLogoutComponent);
+  }
+
+  retrieveLaptops(): void {
+    this.laptopService.getAll().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ key: c.payload.key, ...c.payload.val() })
+        )
+      )
+    ).subscribe(data => {
+      for(let i = 0; i < data.length; i++){
+        if(data[i].published){
+          this.laptops?.push(data[i]);
+        }
+      }     
+    });
   }
 
   gotoTop() {
--- src/app/components/products-card/products-card.component.ts
+++ src/app/components/products-card/products-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Laptop } from 'src/app/models/laptop/laptop.model';
 
 @Component({
   selector: 'app-products-card',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./products-card.component.scss']
 })
 export class ProductsCardComponent {
-
+  @Input() laptop?: Laptop;
 }
